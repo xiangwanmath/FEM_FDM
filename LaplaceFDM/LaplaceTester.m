e62d07bfc3af540ab41b7a689d79562072c6cdd2
Joseph Dingillo <josephdingillo@Josephs-MacBook-Pro.local>
2023-09-28 19:36:57 -0500

    Update - Neumann

--- LaplaceFDM/LaplaceTester.m
+++ LaplaceFDM/LaplaceTester.m
@@ -1,14 +1,11 @@
 gridSize = 50;
 
 steps = [gridSize, gridSize];
-lengths = [4*pi, 4*pi];
+lengths = [8*pi, 8*pi];
 
 test = Laplace2DFD(steps, lengths, lengths(1)/steps(1));
 
-M = test.runSim();
-
+M = test.runSimNeu();
 surf(M);
-
 % Look up sparse matrices for effiency 
-% Log(err) = Log(h)
-% Find regression
+
